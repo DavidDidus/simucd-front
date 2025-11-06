df87e25c3b99bf670834e5b91737bdd0fbcf1a99
DavidDidus <87795619+DavidDidus@users.noreply.github.com>
2025-11-06 16:16:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   "/api",
   createProxyMiddleware({
-    target: "http://simucd-back:8000",
+    target: "http://simucd-back:8080",
     changeOrigin: true,
     pathRewrite: { "^/api": "/api" } // mantiene /api en la ruta
   })
